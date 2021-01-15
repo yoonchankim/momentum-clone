59e69f0dcd8afd0d5b296825d3ae791b1cd6f1b7
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-01-16 05:10:02 +0900

    clock part done

---
+++ clock.js
@@ -0,0 +1,13 @@
+const clockContanier=document.querySelector(".js-clock")
+const clockTitle=document.querySelector("h1")
+function getTime(){
+    const date=new Date();
+    const minutes=date.getMinutes();
+    const hours=date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText=`${hours<10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}` : minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
+}
+function init(){
+    setInterval(getTime,1000);
+}
+init();
